64e376f8717088f554247eb98b61e0d8a8372075
Sean Spaniel <sean.spaniel@clear-measure.com>
2025-06-28 17:21:10 -0400

    initial implementation

---
+++ src/Dotnet.RAG/Dotnet.RAG.Console/Embeddings/PhotoEmbeddingSearchResultItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Dotnet.RAG.Console.Embeddings;
+
+public class PhotoEmbeddingSearchResultItem
+{
+    public string PhotoPath { get; set; } = string.Empty;
+
+    public float Distance { get; set; }
+}
